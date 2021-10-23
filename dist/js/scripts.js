338a5ed4ac1b05819654e32881df0ca13defd072
KJEKcode <kurtiskovel@gmail.com>
2021-10-23 14:47:29 -0400

    basic build and style

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,2 +1,21 @@
-// app js
 "use strict";
+
+// app js
+window.addEventListener("DOMContentLoaded", function () {
+  // MODE BUTTON CLICK - DARK MODE
+  var modeButton = document.getElementById("dark-mode"),
+      modeIcon = modeButton.querySelector(".bx");
+  modeButton.addEventListener("click", function () {
+    if (!document.body.classList.contains("dark-mode")) {
+      document.body.classList.add("dark-mode");
+      modeIcon.classList.remove("bx-moon");
+      modeIcon.classList.add("bx-sun");
+      document.querySelector(".app-banner__button-text").innerHTML = "light";
+    } else if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      modeIcon.classList.remove("bx-sun");
+      modeIcon.classList.add("bx-moon");
+      document.querySelector(".app-banner__button-text").innerHTML = "dark";
+    }
+  });
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
 // app js
+window.addEventListener("DOMContentLoaded", () => {
+  
+  // MODE BUTTON CLICK - DARK MODE
+  const modeButton = document.getElementById("dark-mode"),
+  modeIcon =  modeButton.querySelector(".bx");
+  modeButton.addEventListener("click", () => {
+    if (!document.body.classList.contains("dark-mode")) {
+      document.body.classList.add("dark-mode");
+      modeIcon.classList.remove("bx-moon");
+      modeIcon.classList.add("bx-sun");
+      document.querySelector(".app-banner__button-text").innerHTML = "light";
+    } else if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      modeIcon.classList.remove("bx-sun");
+      modeIcon.classList.add("bx-moon");
+      document.querySelector(".app-banner__button-text").innerHTML = "dark";
+    }
+  });
+ 
+
+});
+
+
